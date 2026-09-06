cc4b0ba86f56dbe263dea1d83a45e99d8a920a6f
Nick Chatzikos <chatzikos@proton.me>
2026-09-06 14:46:57 +0300

    Implement product modal

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,6 +3,25 @@ import { products } from "../js/products.js";
 export const home = () => {
     Alpine.store('products', products);
 
+    Alpine.store('viewProduct', (product) => {
+        const modal = document.getElementById('product-modal');
+        const dismissBtn = document.querySelector('.modal-top-bar a');
+        Alpine.$data(modal).product = product;
+        modal.showModal();
+        gsap.from(modal, {
+            y: 20,
+            scale: 0.95,
+            ease: 'power2.out',
+            duration: 0.3,   
+        })
+
+        dismissBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            modal.close();
+        });
+
+    });
+
     gsap.from('#home-section', {
         y: 12,
         ease: 'power2.out',
